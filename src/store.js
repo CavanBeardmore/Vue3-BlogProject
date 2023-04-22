1a1b4624a1918075ad97654385168c4d9a4acf12
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-04-22 20:57:28 +0100

    Added content and added mutation
    added content to the  accountview page which shows the activeusers details and when the active user is an admin they can view all users and delete accounts, also added a mutation to support the account deletion and to ensure user can't delete their own account using this and to display appropriate error messages depending on the username entered

--- src/store.js
+++ src/store.js
@@ -1,9 +1,28 @@
 import { createStore } from 'vuex';
 
 const store = createStore({
-    state: {title: 'blog-posts'},
+    state: {users: [{usern: 'Gary', email: 'gary@hotmail.com', passw: '12345678', acctype: 'user'},
+                    {usern: 'Phil', email: 'phil@hotmail.com', passw: 'kangaroo123', acctype: 'creator'},
+                    {usern: 'John', email: 'john@hotmail.com', passw: 'galapagosketchup1!', acctype: 'admin'}],
+            activeUser: {usern: 'John', email: 'john@hotmail.com', passw: 'galapagosketchup1!', acctype: 'admin'},
+            errorMsg: ''
+            },
     getters: {},
-    mutations: {},
+    mutations: {
+        DELETE_USER(state, selectedUser) {
+            if (state.activeUser.usern !== selectedUser) {
+                const filteredUsers = state.users.filter((user) => user.usern !== selectedUser)
+                if (filteredUsers.length === state.users.length) {
+                    state.errorMsg = 'This user does not exist, please change this and try again.'
+                } else {
+                    state.users = filteredUsers
+                    state.errorMsg = 'User deleted!'
+                }
+            } else if (state.activeUser.usern === selectedUser){
+                state.errorMsg = 'You have entered your own username, please change this and try again.'
+            } 
+        }
+    },
     actions: {}
 })
 
--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,5 +1,40 @@
 <template>
-  <p> Your account </p>
+  <h3> Your account </h3>
+  <p>
+    Username: {{activeUser.usern}}
+  </p>
+  <p>
+    Email: {{activeUser.email}}
+  </p>
+  <p>
+    Password: {{activeUser.passw}}
+  </p>
+  <p>
+    Account type: {{activeUser.acctype}}
+  </p>
+  <br>
+  <h3> All accounts </h3>
+  <div v-if="activeUser.acctype === 'admin'">
+    <div v-for="user in users" :key="user.usern">
+      <p>
+        Username: {{user.usern}}
+      </p>
+      <p>
+        Email: {{user.email}}
+      </p>
+      <p>
+        Password: {{user.passw}}
+      </p>
+      <p>
+        Account type: {{user.acctype}}
+      </p>
+      <br>
+    </div>
+    <h6> Enter username of the user you wish to delete. </h6>
+    <p> {{errorMsg}} </p>
+    <input type="text" v-model="selectedUser" placeholder="Username">
+    <button @click="deleteUser"> Delete user </button>
+  </div>
 </template>
 
 <script>
@@ -8,9 +43,22 @@ import { ref, computed } from 'vue';
 
 export default {
   setup() {
-    const store = useStore()
+    const store = useStore();
 
-  
+    const users = computed(() => store.state.users);
+
+    const activeUser = computed(() => store.state.activeUser)
+
+    const selectedUser = ref('')
+
+    const errorMsg = computed(() => store.state.errorMsg)
+
+    function deleteUser() {
+      store.commit('DELETE_USER', selectedUser.value)
+      selectedUser.value = ''
+    }
+
+    return {users, selectedUser, activeUser, errorMsg, deleteUser}
     
   }
 }
