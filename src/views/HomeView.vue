533ee817d6ca7ede95ec4210b76755b8d475c556
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-05-11 19:57:07 +0100

    POSTS, AND STORE
    added content to postsview and added posts to the store

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="home">
-    <h1> Hello World </h1>
-    {{$store.state.title}}
+    <h1> Blog Project! </h1>
   </div>
 </template>
 
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,5 +1,18 @@
 <template>
-  <p> Posts! </p>
+  <div class="back">
+    <header> 
+      <h2> Blog Posts </h2>
+    </header>
+    <div class="post-back">
+      <div v-for="post in posts" :key="post.title" class="post-tile">
+        <h3>{{post.title}}</h3>
+        <p>{{post.content}}</p>
+        <div v-if="user.acctype === 'admin'">
+          <p>POST ID: {{post.id}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,12 +23,23 @@ export default {
   setup() {
     const store = useStore()
 
+    const posts = computed(() => store.state.posts)
 
-    
+    const user = computed(() => store.state.signedIn)
+
+    return {
+      posts,
+      user
+    }
   }
 }
 </script>
 
 <style>
-
+  .post-tile {
+    border-radius: 10px;
+    background-color: aquamarine;
+    width: 70%;
+    margin: auto;
+  }
 </style>
