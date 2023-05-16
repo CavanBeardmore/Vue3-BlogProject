b0aece2b122805e8d37bb494e8086c3abbf57ec0
CavanBeardmore <106985709+CavanBeardmore@users.noreply.github.com>
2023-05-16 08:59:43 +0100

    search function, errors and comments
    added more comments to divide up code. added a search function that allows for certain criteria to be search for (currently only creator works, will be adding in title and tags soon), and also added error messages for search and post creation

--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -1,9 +1,22 @@
 <template>
   <div class="back">
 
+    <!-- create post section -->
     <div class="creator-controls" v-if="user.acctype === 'creator'">
-      <button @click="toggleCreate">Create Post</button>
+      <div v-show="!create">
+        <button @click="toggleCreate">Create Post</button>
+      </div>
       <div v-show="create">
+        <button @click="toggleCreate">View Posts</button>
+        <br>
+        <br>
+        <button @click="toggleRequirements"> View Requirements </button>
+        <div v-show="requirements">
+          <p> The title is 40 characters maximum and more than 5 characters minimum. </p>
+          <p> The content section is 6000 characters maximum and 500 characters minimum. </p>
+          <p> There must be a minimum of one tag and a maximum of 3 tags.</p>
+        </div>
+        <br>
         <br>
         <button @click="publishPost(title, content, tags)">Publish Post</button>
         <br>
@@ -23,11 +36,36 @@
           <li v-for="tag in tags" :key="tag" class="tag-list">
             {{tag}}
           </li>
-          <h5 style="color: red">{{tagError}}</h5>
         </div>
+        <h5 style="color: red">{{tagError}}</h5>
+        <h5 style="color: green">{{postMessage}}</h5>
+        <h5 style="color: red">{{postError}}</h5>
       </div>
     </div>
-    
+
+    <!-- search -->
+    <div>
+      <select name="search-criteria" id="search-criteria" v-model="criteria">
+        <option value="Tag" selected>Tag</option>
+        <option value="Title">Title</option>
+        <option value="Creator">Creator</option>
+      </select>
+      <input type="text" placeholder="" v-model="searchInput">
+      <br>
+      <br>
+      <button @click="searchFunc">Search</button>
+      <br>
+      <p style="color: red">{{searchError}}</p>
+      <div v-for="post in filteredSearch" :key="post.id">
+        <p>{{post.title}}</p>
+        <p>{{post.content}}</p>
+        <p>{{post.creator}}</p>
+        <p>{{post.tags}}</p>
+        <p>{{post.id}}</p>
+      </div>
+    </div>
+
+    <!-- posts section -->
     <div class="post-back" v-show="!create">
       <div v-for="post in posts" :key="post.title" class="post-tile">
         <h3>{{post.title}}</h3>
@@ -42,6 +80,7 @@
         </div>
       </div>
     </div>
+
   </div>
 </template>
 
@@ -52,30 +91,67 @@ import { hasLength, lessThan, doesExist, moreThan } from '../funcs';
 
 export default {
   setup() {
+    //use 
     const store = useStore()
 
     //computed
     const posts = computed(() => store.state.posts)
     const user = computed(() => store.state.signedIn)
 
-    //refs
+    //boolean refs
     const create = ref(false)
+    const requirements = ref(false)
+
+    //input refs
     const title = ref('')
     const content = ref('')
     const tag = ref('')
-    const tags = ref([])
-    const tagError = ref('')
+    const searchInput = ref('')
+    const criteria = ref('Tag')
 
+    //error & message refs
+    const tagError = ref('')
+    const postError = ref('')
+    const postMessage = ref('')
+    const searchError = ref('')
+
+    //array ref
+    const tags = ref([])
+    const filteredSearch = ref()
+
+    //toggle funcs
     function toggleCreate() {
       create.value = !create.value
     }
+    function toggleRequirements() {
+      requirements.value = !requirements.value
+    }
 
+    function searchFunc() {
+      if (criteria.value === 'Creator') {
+        const creatorFiltered = posts.value.filter((post) => post.creator === searchInput.value)
+        console.log(creatorFiltered.length)
+        filteredSearch.value = creatorFiltered
+        if (creatorFiltered.length === 0) {
+        searchError.value = 'No results found'
+        }
+      } 
+    }
+
+    //publish post function takes the v-modeled refs as arguments and uses import funcs to check their lengths
+    //if they pass then they
     function publishPost(title, content, tagsArr) {
       if (hasLength(title) && hasLength(content) && hasLength(tagsArr)) {
         if (lessThan(title, 40) && moreThan(title, 5) && lessThan(content, 6000) && moreThan(content, 500) && lessThan(tagsArr, 3)) {
           store.commit('PUBLISH_POST', { title, content, tagsArr })
+          postMessage.value = 'Post has been published!'
+          postError.value = ''
         }
-      } 
+      } else {
+        postMessage.value = ''
+        postError.value = 'Post does not meet requirements.'
+        requirements.value = true
+      }
     }
 
     function addTag() {
@@ -104,7 +180,16 @@ export default {
       tag,
       tags,
       addTag,
-      tagError
+      tagError,
+      requirements,
+      toggleRequirements,
+      postError,
+      postMessage,
+      searchInput,
+      criteria,
+      searchFunc,
+      filteredSearch,
+      searchError
     }
   }
 }
